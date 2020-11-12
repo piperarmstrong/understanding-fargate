8215e1497bd1a706b536549fe4a39228cb98f128
Piper Vasicek <38051163+piperarmstrong@users.noreply.github.com>
2020-11-12 09:00:56 -0700

    Initialize least-privilege

---
+++ terraform-iac/dev/app/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = "0.12.26"
+  backend "s3" {
+    bucket         = "terraform-state-storage-977306314792"
+    dynamodb_table = "terraform-state-lock-977306314792"
+    key            = "hw-fargate-api-dev/app.tfstate"
+    region         = "us-west-2"
+  }
+}
+
+provider "aws" {
+  version = "~> 2.42"
+  region  = "us-west-2"
+}
+
+variable "image_tag" {
+  type = string
+}
+
+module "app" {
+  source                           = "../../modules/app/"
+  env                              = "dev"
+  image_tag                        = var.image_tag
+  codedeploy_termination_wait_time = 0
+  deploy_test_postman_collection   = "../../../.postman/hw-fargate-api.postman_collection.json"
+  deploy_test_postman_environment  = "../../../.postman/dev-tst.postman_environment.json"
+}
+
+output "url" {
+  value = module.app.url
+}
+
+output "codedeploy_app_name" {
+  value = module.app.codedeploy_app_name
+}
+
+output "codedeploy_deployment_group_name" {
+  value = module.app.codedeploy_deployment_group_name
+}
+
+output "codedeploy_appspec_json_file" {
+  value = module.app.codedeploy_appspec_json_file
+}
---
+++ terraform-iac/modules/setup/setup.tf
@@ -0,0 +1,82 @@
+variable "env" {
+  type = string
+}
+
+variable "some_secret" {
+  type = string
+}
+
+locals {
+  name = "hw-fargate-api"
+}
+
+resource "aws_ssm_parameter" "some_secret" {
+  name  = "/${local.name}/${var.env}/some-secret"
+  type  = "SecureString"
+  value = var.some_secret
+}
+
+module "my_ecr" {
+  source = "github.com/byu-oit/terraform-aws-ecr?ref=v1.1.1"
+  name   = "${local.name}-${var.env}"
+}
+
+data "aws_iam_user" "github_actions" {
+  user_name = "GitHub-Actions"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "deploy" {
+  name                 = "${local.name}-deploy"
+  permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/PowerUserPolicy"
+  assume_role_policy   = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowIamUserAssumeRole",
+            "Effect": "Allow",
+            "Action": "sts:AssumeRole",
+            "Principal": {"AWS": "${data.aws_iam_user.github_actions.arn}"}
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "deploy_policy" {
+  name = "deploy_policy"
+  role = aws_iam_role.deploy.id
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "s3:*",
+          "elasticloadbalancing:*",
+          "route53:*",
+          "ecr:*",
+          "dynamodb:*",
+          "iam:*",
+          "ecs:*",
+          "logs:*",
+          "codedeploy:*",
+          "ssm:*",
+          "acm:*",
+          "vpc:*",
+          "ec2:*",
+          "application-autoscaling:*",
+          "lambda:*",
+          "cloudwatch:*"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
+      }
+    ]
+  }
+  EOF
+}
+
