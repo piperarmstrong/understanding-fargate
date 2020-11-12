ffd6da6d76c15296b32e1cdd46ccbfa7427055f1
Piper Vasicek <38051163+piperarmstrong@users.noreply.github.com>
2020-11-12 09:01:03 -0700

    Initialize dependabot/npm_and_yarn/src/dev/standard-16.0.2

---
+++ terraform-iac/prd/app/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = "0.12.26"
+  backend "s3" {
+    bucket         = "terraform-state-storage-539738229445"
+    dynamodb_table = "terraform-state-lock-539738229445"
+    key            = "hw-fargate-api-prd/app.tfstate"
+    region         = "us-west-2"
+  }
+}
+
+provider "aws" {
+  version = ">= 3.0"
+  region  = "us-west-2"
+}
+
+variable "image_tag" {
+  type = string
+}
+
+module "app" {
+  source                           = "../../modules/app/"
+  env                              = "prd"
+  image_tag                        = var.image_tag
+  codedeploy_termination_wait_time = 15
+  deploy_test_postman_collection   = "../../../.postman/hw-fargate-api.postman_collection.json"
+  deploy_test_postman_environment  = "../../../.postman/prd-tst.postman_environment.json"
+}
+
+output "url" {
+  value = module.app.url
+}
+
+output "codedeploy_app_name" {
+  value = module.app.codedeploy_app_name
+}
+
+output "codedeploy_deployment_group_name" {
+  value = module.app.codedeploy_deployment_group_name
+}
+
+output "codedeploy_appspec_json_file" {
+  value = module.app.codedeploy_appspec_json_file
+}
